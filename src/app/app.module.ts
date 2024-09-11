2fbef8aa43459b159368a3450e5d7001a5705c6a
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-09-11 15:06:45 +0700

    build route and add pages login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+
+let components:any = [
+
+]
+
+let pages = [
+  AppComponent,
+  LoginComponent
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [...components,...pages],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [...pages]
 })
 export class AppModule { }
