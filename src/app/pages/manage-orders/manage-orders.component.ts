45ea5add9f5d40ad99518bbdfacccaafe67f85a9
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2024-12-04 23:14:34 +0700

    u

--- src/app/pages/manage-orders/manage-orders.component.ts
+++ src/app/pages/manage-orders/manage-orders.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-orders.component.scss']
 })
 export class ManageOrdersComponent {
+  tabs = [
+    // {
+    //   name: 'Tất cả',
+    //   icon: '',
+    //   status:-1
+    // },
+    {
+      name: 'Chờ xác nhận',
+      icon: 'double-right',
+      status:0
+    },
+    {
+      name: 'Đang pha chế',
+      icon: 'double-right',
+      status:1
+    },
+    {
+      name: 'Đang giao hàng',
+      icon: 'double-right',
+      status:2
+    },
+    {
+      name: 'Đã nhận',
+      icon: 'double-right',
+      status:3
+    },
+    {
+      name: 'Hủy',
+      icon: 'double-right',
+      status:4
+    }
+  ];
+
+  count_status = [0,0,0,0,0]
+
+  changeStatus(status:any){
+
+    // if (status==-1)
+    // {
+    //   this.bills_v=[...this.bills]
+    // }else{
+    //   this.bills_v=this.bills.filter((bill:any)=>bill.status==status)
+    // }
+  }
 
 }
