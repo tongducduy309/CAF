d5f76d2ecc4c3a9d43a0a08a54c280e9246dc695
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2025-09-23 01:57:58 +0700

    update file url env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     API_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/",
     // API_URL:"",
     BASE_URL:"http://localhost:8080/api/v1/",
-    FILE_URL:"http://localhost:8080/api/v1/files/",
-    
+    // FILE_URL:"http://localhost:8080/api/v1/files/",
+    FILE_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/files/",
   }
 };
