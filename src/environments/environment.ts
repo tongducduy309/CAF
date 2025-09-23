a046e850c47c1df83f31dbcafa090a54e348a58d
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2025-09-23 10:42:32 +0700

    change connect url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   
   variable_global:{
-    // API_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/",
-    API_URL:"",
+    API_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/",
+    // API_URL:"",
     BASE_URL:"http://localhost:8080/api/v1/",
     FILE_URL:"http://localhost:8080/api/v1/files/",
     // FILE_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/files/",
