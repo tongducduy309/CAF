ae637f2452ebd35a9fad2ae1d48015bc418aaf3c
tongducduy309 <119146803+tongducduy309@users.noreply.github.com>
2025-09-26 19:32:21 +0700

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  
+  GOOGLE_CLIENT_ID: '1037670789577-2l8vf8ka19vfar6tmiesg5ltvbbud9s0.apps.googleusercontent.com',
   variable_global:{
-    API_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/",
+    API_URL:"https://api-jsb-coffeestore.onrender.com/",
     // API_URL:"",
-    BASE_URL:"http://localhost:8080/api/v1/",
+    BASE_URL:"http://localhost:8080/",
     FILE_URL:"http://localhost:8080/api/v1/files/",
     // FILE_URL:"https://api-jsb-coffeestore.onrender.com/api/v1/files/",
   }
